b2d6f718c099001cb4e8fc6ecd6c0a33c4933f67
Joshua Heeren <113561815+Jmaxtrax@users.noreply.github.com>
2024-04-02 18:38:17 -0500

    Tutorial pt. 7, Sessions, counting how many times you visited a website

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,6 +20,10 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     search_query = 'the'  
     num_books_containing_query = Book.objects.filter(title__icontains=search_query).count()
 
@@ -28,6 +32,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
